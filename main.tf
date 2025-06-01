d5646a1039908632f688c1149a7ed07b20a6ea50
TeenaB <89134829+TeenaB@users.noreply.github.com>
2025-06-01 16:18:42 +0000

    tf file

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "teena_23/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {
+  host    = "npipe:////.//pipe//docker_engine"
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
